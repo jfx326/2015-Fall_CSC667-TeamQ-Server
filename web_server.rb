b1bfaae2263653607a9299e4ccf4fae39dea9138
dilshan <dilshan.raja@gmail.com>
2015-09-12 21:32:05 -0700

    Add load statuses

--- web_server.rb
+++ web_server.rb
@@ -15,12 +15,15 @@ module WebServer
 
       @options = options
 
+      puts "Loading Configuration..."   
       httpd_file = File.open('config/httpd.conf', 'rb')
       mime_file = File.open('config/mime.types', 'rb')
 
       @options[:httpd_conf] = HttpdConf.new(httpd_file)
+      puts "Finished loading httpd_conf..."
 
       @options[:mime_types] = MimeTypes.new(mime_file)
+      puts "Finished loading MIME Types..."
 
       httpd_file.close
       mime_file.close
@@ -33,6 +36,7 @@ module WebServer
       port = options[:httpd_conf].port || DEFAULT_PORT
       @server ||= TCPServer.open(port)
 
+      puts "Starting server on localhost:#{port}\n\n"
       loop do 
         socket = @server.accept
 

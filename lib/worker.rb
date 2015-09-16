5154add8832989a17d39b7eede307d5b654015be
dilshan <dilshan.raja@gmail.com>
2015-09-15 21:10:04 -0700

    Initial multitasking functionality

--- lib/worker.rb
+++ lib/worker.rb
@@ -8,10 +8,29 @@ module WebServer
   class Worker
     # Takes a reference to the client socket and the logger object
     def initialize(client_socket, server=nil)
+      @socket = client_socket
+      @server = server
+
+      process_request
     end
 
     # Processes the request
     def process_request
+      # puts "Incoming connection..."
+
+      #TODO: THIS NEEDS ERROR CHECKING!!
+      request = Request.new(@socket)
+      puts "REQUEST: #{request.http_method} #{request.uri}\n"
+      # puts "Request created..."
+      resource = Resource.new(request, @server.options[:httpd_conf], @server.options[:mime_types])
+      response = Response::Factory.create(resource)
+      # puts "Response created..."
+
+      @socket.write response.to_s
+      # puts "Response transmitted..."
+
+      @socket.close
+      # puts "Connection terminated\n\n"
     end
   end
 end

997cf94a6ca7cfc8d97696c15f8b3595d3ce4386
Joseph Fernandez <jfx326@live.com>
2015-09-21 01:13:20 -0700

    Added implementation for Logger. - lib/logger.rb   - Instances of Logger for each connection will be called at Worker,     at "process_request"
      - METHOD RUNDOWN:
        - initialize
          - opens log file at path defined at httpd_conf.conf to log request,
            response
          - prints to stdout, writes to log time of connection,
            "Connection opened" prompt
          - TODO: clarification of "I include the option :echo...?"
            See comments.
        - log
          - logs made using info from request, response
            (see lib/response/base.rb)
          - prints to stdout, writes to log basic HTTP request info
            (HTTP version, URI, headers), server response code
        - close
          - "Connection closed" prompt printed and written before
            closing log file
    
    - lib/worker.rb
      - process_request: added Logger
    
    - lib/response.rb
      - no code modified...?
    
    Committed by jfx326, 2015-09-21, 1:26 AM PDT.

--- lib/logger.rb
+++ lib/logger.rb
@@ -5,17 +5,51 @@ module WebServer
     # you may want to specify.  I include the option :echo to 
     # allow me to decide if I want my server to print out log
     # messages as they get generated
+    
+    # TODO: clarify "I include option :echo...?"
+    
+    # log_file_path already defined at httpd_conf.conf
     def initialize(log_file_path, options={})
+      @options = options
+      
+      unless @options.has_key?(:echo) # please see TODO above
+        @options[:echo] = File.open(log_file_path, 'a')
+      end
+      
+      message = Time.now.strftime('%a, %e %b %Y %H:%M:%S %Z') + "\r\n"
+      message << "Connection opened.\r\n"
+      puts message
+      @options[:echo].puts message
     end
 
     # Log a message using the information from Request and 
     # Response objects
     def log(request, response)
+      message = "User #{request.user_id} requested with "
+      message << "HTTP/#{request.version} at #{request.uri}:\r\n"
+      message << "Headers:\r\n"
+      request.headers.each do |header, value|
+        message << "#{header}: #{value}\r\n"
+      end
+      
+      message << "Options:\r\n"
+      request.params.each do |option, value|
+        message << "#{option}: #{value}\r\n"
+      end
+      
+      message << "Server responded with #{response.code}.\r\n"
+      
+      puts message
+      @options[:echo].puts message
     end
 
     # Allow the consumer of this class to flush and close the 
     # log file
     def close
+      message = "Connection closed.\r\n"
+      puts message
+      @options[:echo].puts message
+      @options[:echo].close
     end
   end
 end

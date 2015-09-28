dcf0046bbce385cb12fa267601e5e34052fa5fde
dilshan <dilshan.raja@gmail.com>
2015-09-27 17:58:29 -0700

    Remove status messages on worker

--- lib/worker.rb
+++ lib/worker.rb
@@ -15,30 +15,22 @@ module WebServer
     end
 
     # Processes the request
-    # TODO: okay to clear out commented puts?
     def process_request
-      puts "Incoming connection..."
-      
       # log_file_path already defined at httpd_conf.conf
       log_file_path = @server.options[:httpd_conf].log_file
 
       #TODO: THIS NEEDS ERROR CHECKING!!
       logger = Logger.new(log_file_path)
+
       request = Request.new(@socket)
-      # puts "REQUEST: #{request.http_method} #{request.uri}\n"
-      # puts "Request created..."
       resource = Resource.new(request, @server.options[:httpd_conf], @server.options[:mime_types])
       response = Response::Factory.create(resource)
-      # puts "Response created..."
-      
-      logger.log(request, response)
 
+      logger.log(request, response)
       @socket.write response.to_s
-      puts "Response transmitted..."
 
       @socket.close
-      puts "Connection terminated\n\n"
-      
+
       logger.close
     end
   end

d0e37b25469869a57bbdf269c24b8ebf164b6fab
dilshan <dilshan.raja@gmail.com>
2015-09-27 17:56:44 -0700

    500 Server Error object

--- lib/response/server_error.rb
+++ lib/response/server_error.rb
@@ -3,6 +3,26 @@ module WebServer
     # Class to handle 500 errors
     class ServerError < Base
       def initialize(resource, options={})
+        super(resource)
+        @code = 500
+
+        @body = "<html><body><h1>500 - Internal Server Error</h1></body></html>"
+      end
+
+      def message
+        msg = "Content-Type: #{content_type}\n"
+        msg << "Content-Length: #{content_length}\n"
+        msg << "Connection: close\n"
+        msg << "\r\n"
+        msg << @body
+      end
+
+      def content_type
+        return "text/html"
+      end
+
+      def content_length
+        return @body.length
       end
     end
   end

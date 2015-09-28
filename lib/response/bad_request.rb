cc43b9cead2c0ba0a80d5fa1f339b72d4f20978d
dilshanraja <dilshan.raja@gmail.com>
2015-09-28 02:42:04 -0700

    Bad Request and Forbidden repsonse objects

--- lib/response/bad_request.rb
+++ lib/response/bad_request.rb
@@ -3,6 +3,30 @@ module WebServer
     # Class to handle 400 responses
     class BadRequest < Base
       def initialize(resource, options={})
+        super(resource)
+        @code = 204
+
+        @body = "<html><body><h1>400 - Bad Request</h1></body></html>"
+      end
+
+      def to_s
+        s = head
+        s << "Connection: close\n"
+        s << "\r\n"
+
+        return s
+      end
+
+      def message
+        msg = "Content-Type: text/html\n"
+        msg << "Content-Length: #{content_length}\n"
+        msg << "Connection: close\n"
+        msg << "\r\n"
+        msg << @body
+      end
+
+      def content_length
+        return @body.length
       end
     end
   end
--- lib/response/forbidden.rb
+++ lib/response/forbidden.rb
@@ -5,10 +5,27 @@ module WebServer
       def initialize(resource, options={})
         super(resource)
         @code = 403
+
+        @body = "<html><body><h1>403 - Forbidden</h1></body></html>"
+      end
+
+      def to_s
+        s = head
+        s << message
+
+        return s
       end
 
       def message
-        return ''
+        msg = "Content-Type: text/html\n"
+        msg << "Content-Length: #{content_length}\n"
+        msg << "Connection: close\n"
+        msg << "\r\n"
+        msg << @body
+      end
+
+      def content_length
+        return @body.length
       end
     end
   end

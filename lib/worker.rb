8793312314cab104751e45061698ecc673b309be
Joseph Fernandez <jfx326@live.com>
2015-09-27 17:28:05 -0700

    Trivially modified lib/worker.rb. - require_relative 'logger' set - remove puts for connection statuses (incoming, transmission, close, etc.)
    TODO: reflect changes in master, error checking in worker
    
    Committed by jfx326, 2015-09-27, 5:29 PM PDT.

--- lib/worker.rb
+++ lib/worker.rb
@@ -1,5 +1,6 @@
 require_relative 'request'
 require_relative 'response'
+require_relative 'logger'
 
 # This class will be executed in the context of a thread, and
 # should instantiate a Request from the client (socket), perform
@@ -15,29 +16,19 @@ module WebServer
     end
 
     # Processes the request
-    # TODO: okay to clear out commented puts?
     def process_request
-      puts "Incoming connection..."
-      
-      # log_file_path already defined at httpd_conf.conf
       log_file_path = @server.options[:httpd_conf].log_file
 
       #TODO: THIS NEEDS ERROR CHECKING!!
       logger = Logger.new(log_file_path)
       request = Request.new(@socket)
-      # puts "REQUEST: #{request.http_method} #{request.uri}\n"
-      # puts "Request created..."
       resource = Resource.new(request, @server.options[:httpd_conf], @server.options[:mime_types])
       response = Response::Factory.create(resource)
-      # puts "Response created..."
       
       logger.log(request, response)
-
+      
       @socket.write response.to_s
-      puts "Response transmitted..."
-
       @socket.close
-      puts "Connection terminated\n\n"
       
       logger.close
     end

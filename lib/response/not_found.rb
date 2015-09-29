01bfedcea9716c23b355d109bb117259d2632170
dilshanraja <dilshan.raja@gmail.com>
2015-09-29 02:36:29 -0700

    404 class amendment

--- lib/response/not_found.rb
+++ lib/response/not_found.rb
@@ -8,9 +8,20 @@ module WebServer
 
         @body = "<html><body><h1>404 - Resource Not Found</h1></body></html>"
       end
+      
+      def to_s
+        s = head
+        s << message
 
-      def content_type
-        return 'text/html'
+        return s
+      end
+
+      def message
+        msg = "Content-Type: text/html\n"
+        msg << "Content-Length: #{content_length}\n"
+        msg << "Connection: close\n"
+        msg << "\r\n"
+        msg << @body
       end
 
       def content_length
